bc05517f1cb50b25904ff7773e026f229ff7eb33
Dev Patel <devptl841806@gmail.com>
2024-05-26 14:44:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,61 +6,45 @@ import json
 
 app = Flask(__name__)
 
-# Function to authenticate using OAuth2 credentials
+
 def authenticate():
-    # Authenticate using the OAuth2 credentials
     sc = OAuth2(None, None, from_file='json/ouath2.json')
     return sc
 
-
-# # Define a custom filter for multiplication
 # def multiply(value, factor):
 #     return value * factor
-
-# # Add the custom filter to the Jinja2 environment
 # app.jinja_env.filters['multiply'] = multiply
 
 
 def fetch_league_info(league_id):
-    # Authenticate
+
     sc = authenticate()
     
-    # Access the NBA game
+
     gm = yfa.Game(sc, 'nba')
 
-    # Access the league using the provided league ID
     lg = gm.to_league(league_id)
 
-    # Get the standings for the league
     standings = lg.standings()
 
-    # Prepare data
     data = {"standings": standings}
 
-    # Initialize a dictionary to store team rosters
     team_data = {}
 
-    # Iterate over each team in the standings
     for team_standings in standings:
         team_key = team_standings['team_key']
-        # Access the team associated with the team key
         team = lg.to_team(team_key)
-        # Get the roster for the team
         team_roster = team.roster()
-        # Store the roster in the team_data dictionary
         team_data[team_key] = team_roster
 
-    # Save data to JSON file
     with open('json/fantasy_data.json', 'w') as json_file:
         json.dump(data, json_file, indent=4)
         
-    # Save team roster data to JSON file
     with open('json/fantasy_team_data.json', 'w') as json_file:
         json.dump(team_data, json_file, indent=4)
 
 
 
-# Function to read JSON data from file
 def read_json_data():
     with open('json/fantasy_data.json', 'r') as json_file:
         data = json.load(json_file)
